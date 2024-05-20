f402ec681e528391c4cab27b9f717316f2781366
th-guasti <th.guasti@gmail.com>
2024-05-20 14:47:19 -0300

    implementando o layout

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,38 @@ class MyApp extends StatelessWidget {
             'Tarefas',
             style: TextStyle(color: Colors.white),
           ),
-          backgroundColor: Colors.blue[700],
+          backgroundColor: Colors.blue,
         ),
-        body: Container(),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 140,
+              ),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text('Aprender Flutter'),
+                    ElevatedButton(
+                        onPressed: () {
+                          print('seta clicada');
+                        },
+                        child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
         ),
