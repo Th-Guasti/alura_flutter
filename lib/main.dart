e9928e898839dd558b8409fb0638117fb545ca12
th-guasti <th.guasti@gmail.com>
2024-05-20 15:10:14 -0300

    utilizando o stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, avoid_print
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -25,39 +25,56 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                color: Colors.blue,
-                height: 140,
-              ),
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    Text('Aprender Flutter'),
-                    ElevatedButton(
-                        onPressed: () {
-                          print('seta clicada');
-                        },
-                        child: Icon(Icons.arrow_drop_up))
-                  ],
-                ),
-              )
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Task('Aprender flutter'),
+            Task('Andar de bike'),
+            Task('Meditar'),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
         ),
+      ),
+    );
+  }
+}
+
+class Task extends StatelessWidget {
+  final String nome;
+  const Task(this.nome, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 140,
+          ),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.black26,
+                  width: 72,
+                  height: 100,
+                ),
+                Text(nome),
+                ElevatedButton(
+                    onPressed: () {
+                      print('seta clicada');
+                    },
+                    child: Icon(Icons.arrow_drop_up))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
